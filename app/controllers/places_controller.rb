f24823e4814dfddd00cddae32060ee780dd13ce1
Tomas Espina <tespintom@icloud.com>
2017-08-16 19:30:50 +0000

    connected user to places and fixed niil users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
+
+
   def index
     @places = Place.paginate(:page => params[:page], :per_page =>2)
   end
@@ -9,11 +13,11 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
 
   end 
-  
+
   private
 
   def place_params
