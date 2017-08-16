ecd6f9996994441b69a824ca707c9a8be72366fc
Tomas Espina <tespintom@icloud.com>
2017-08-16 17:14:00 +0000

    change pagination to max of 2 to display box issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.paginate(:page => params[:page], :per_page =>1)
+    @places = Place.paginate(:page => params[:page], :per_page =>2)
   end
 end
