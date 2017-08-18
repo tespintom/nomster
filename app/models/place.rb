dab6e0713dc829df06fa67db813dccde68de0884
Tomas Espina <tespintom@icloud.com>
2017-08-18 23:26:20 +0000

    like comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  belongs_to :comments
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true, length: { minimum: 4 }
