df2e57aaf81e56a0f6e668bd86e20066ddc2a254
Tomas Espina <tespintom@icloud.com>
2017-08-21 02:11:26 +0000

    added carousel and styled the carousel and font and i think i fixed the nav bar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,16 @@
 Rails.application.configure do
 
   config.action_mailer.default_url_options = {host:'nomster-tomas-espina.herokuapp.com'}
-
+ 
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
  
 
   # Settings specified here will take precedence over those in config/application.rb.
